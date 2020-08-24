128161461f47855da2ac795974027da6ddbe656f
hoho9711 <59272865+hoho9711@users.noreply.github.com>
2020-08-24 15:33:42 +0900

    userdata.sql
    10pcs

---
+++ userdata.sql
@@ -0,0 +1,37 @@
+create table userinfo (
+	userid varchar2(15) primary key,
+	username varchar2(20),
+	userpwd varchar2(10),
+	email varchar2(50),
+	phone varchar2(15),
+	address varchar2(100),
+	userdate date default sysdate
+);
+
+
+create table board
+(
+    boardno   number(10) primary key,
+    title     varchar2(100)  not null,
+    content   varchar2(4000) not null,
+    writer varchar2(15) references userinfo(userid),
+    boarddate date   default sysdate,
+    viewcount   number(10)
+)
+
+desc userinfo;
+select * from userinfo;
+
+insert into userinfo values ('dkdl8888', 'T없e맑은I','erer!23232','ii333@daum.net', '010-2323-4444' ,'서울시 강서구');
+insert into userinfo values ('sldms1221', '손흥민', 'tlfj@131','like444@daum.net', '010-1111-4444' ,'서울시 강북구');
+insert into userinfo values ('boom9898', '장미여신','anjgka123!', 'love2388@daum.net', '010-2323-4455' ,'서울시 종로구');
+insert into userinfo values ('gma3333', '뽀대작살', 'alal#999','dktk@naver.com', '010-9797-3434' ,'대전 유성구');
+insert into userinfo values ('hmmm22', 'simonD', 'dkssud@333','simon@naver.com', '010-5555-3543' ,'충북 영동군');
+insert into userinfo values ('zzizon52', 'Esens', 'qkqh9090!@', 'esens@naver.com', '' ,'부산 해운대구');
+insert into userinfo values ('dfewww22', '패션왕', 'yumyum1!', 'king@yahoo.com', '010-3425-4232' ,'서울시 강서구');
+insert into userinfo values ('hanana8944', 'S2러브천사S2','sjssjs9*', 'angel@naver.com', '010-5252-2221' ,'부산 서면');
+insert into userinfo values ('coding554', 'zl존코딩', 'nyo@3322','coder@naver.com', '010-6875-3423' ,'서울 동작구');
+insert into userinfo values ('pizzalover0101', '치킨나라피자공주', 'pilot3#', 'pizza@naver.com', '010-5555-1111' ,'부산 서면');
+
+select * from userinfo
+
