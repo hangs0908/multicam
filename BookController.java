f097961e2ec184433daac26586e98b51f46860b1
hangs0908 <56857925+hangs0908@users.noreply.github.com>
2020-08-26 15:58:44 +0900

    Update BookController.java

--- BookController.java
+++ BookController.java
@@ -1,13 +1,42 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import biz.book.dao.BookDAO;
+import biz.book.service.BookService;
 
 @Controller
 public class BookController {
-	@RequestMapping("/book.do")
-	public String book() {
-		return "book/book_list";
+	
+	@Autowired
+	BookService service;
+	@RequestMapping("/board.do")
+	public String board(HttpServletRequest request) {
+		String lib = request.getParameter("searchOption1");
+		String ti = request.getParameter("searchOption2");
+		String text = request.getParameter("search");
+		if(ti == "publisher") {
+			request.setAttribute("booklist", service.getBookList3(lib, text)); 
+		} else if(ti == "author") {
+			request.setAttribute("booklist",service.getBookList2(lib, text));
+		} else {
+			request.setAttribute("booklist",service.getBookList(lib, text));
+		} 
+		return "/book/book_list";
+	}
+	
+	@RequestMapping(value = "/book.do", method = RequestMethod.GET)
+	public ModelAndView book(int bookKey) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("book", service.getBook(bookKey));
+		mav.setViewName("/book/book_view");
+		return mav;
 	}
 }
