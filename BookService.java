904c94ff085ff5050794df1d48a3b55d760961f0
hangs0908 <56857925+hangs0908@users.noreply.github.com>
2020-08-26 15:56:34 +0900

    Update BookService.java

--- BookService.java
+++ BookService.java
@@ -2,12 +2,42 @@ package biz.book.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import biz.book.dao.BookDAO;
 import biz.book.vo.BookVO;
 
-public interface BookService {
-		BookVO getBook (String title);
-	   List<BookVO> getBookList(String title);
-	   List<BookVO> getBookList2(String author);
-	   List<BookVO> getBookList3(String publisher);
-	   List<BookVO> bookViewList();
+@Service("bookservice")
+public class BookServiceImpl implements BookService{
+
+	@Autowired
+	BookDAO dao;
+
+	@Override
+	public BookVO getBook(int bookKey) {
+		return dao.getBook(bookKey);
+	}
+
+	@Override
+	public List<BookVO> getBookList(String libName, String title) {
+		return dao.getBookList(libName, title);
+	}
+	@Override
+	public List<BookVO> getBookList2(String libName, String author) {
+		return dao.getBookList2(libName, author);
+	}
+	
+	@Override
+	public List<BookVO> getBookList3(String libName, String publisher) {
+		return dao.getBookList3(libName, publisher);
+	}
+
+	@Override
+	public List<BookVO> bookViewList() {
+		return dao.bookViewList();
+	}
+	
+	
+	
 }
